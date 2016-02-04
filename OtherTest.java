84aee76bca48cfbdd055380783113dd0bc249327
汤劲戈 <tjg-6666@163.com>
2016-02-04 23:48:18 +0800

    some other matters needing attention

--- OtherTest.java
+++ OtherTest.java
@@ -7,24 +7,55 @@ public class OtherTest extends BaseDemo {
 		/** 
 		 * From a to b is Widening, so the cast is not required. 
 		 * From b to a is Narrowing. */
+		{
 		int a = 3;
 		long b = a;
 		int c = (int) b;
-		
+		}
+
+		/** can't get t if there's no initialize. */
+		{
 		int t;
-		/** can't get t now, because it has no a initialize. 
-		 * Sample: println(t); */
+		// println(t); is wrong
 		for (int i=0; i<10; i++) {
-			/**can't define an existing variable, like t
-			 * Sample: int t = 0; */
+			/**can't define an existing variable, like t */
+			 // int t = 0; is wrong
+		}
 		}
 		
+		{
 		/**
 		 * any opt with char\byte\short will result in "int".
 		 * so (z+1) should be cast to char. 
 		 */
-		char z = 'a';
-		z = (char) (z+1);
-		println(z);	//print b
+		char a = 'a';
+		a = (char) (a+1);
+		println(a);	//print b
+		}
+		
+		{
+		/** foreach sample */
+		int a[] = new int[3];
+		for (int i=0; i<3; i++) a[i]=i;
+		for(int x : a) println(x); 
+		}
+		
+		{
+		/** label sample
+		 * break label will break the whole literal (include below "while (true)"
+		 * continue label will continue the loop where the label is 
+		 * (in here is "for j ..") */
+		long sum = 0;
+		label:
+			for (int j=0; ; j++) {
+				for (int i=0; ; i++) { 
+					if (i==5)
+						if (sum>30) break label;
+						else continue label;
+					sum += i;
+					println(i+" "+sum+" "+j);
+				}
+			}
+		}
 	}
 }

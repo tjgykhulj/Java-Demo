172e517cd9b77fe22f8024e0d01c2173aec4c147
汤劲戈 <tjg-6666@163.com>
2016-02-04 20:29:13 +0800

    base class

--- BaseDemo.java
+++ BaseDemo.java
@@ -2,7 +2,7 @@ package demo;
 
 public class BaseDemo {
 	/** a base class that contains some useful functions */
-	static void print(Object s) {
+	static void println(Object s) {
 		System.out.println(s);
 	}
 }

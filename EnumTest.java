5276428d5007b13fc02f90455b551d8b39d4f2e4
汤劲戈 <tjg-6666@163.com>
2016-02-12 22:38:05 +0800

    2016/2/12

--- EnumTest.java
+++ EnumTest.java
@@ -5,10 +5,19 @@ public class EnumTest extends BaseDemo{
 	enum Date {
 		Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday 
 	}
+	
+	private static String judge(Date d) {	//enum can work with switch
+		switch (d) {
+		case Saturday:
+		case Sunday: return "Home";
+		default: return "Work";
+		}
+	}
 	public static void main(String args[]) {
 		Date a = Date.Wednesday;
 		println(a);	//print: "Wednesday"
-		for (Date i : Date.values())
-			println(i+" : "+i.ordinal()); //print example : "Monday : 0" 
+		for (Date i : Date.values())			
+			println(i+" : "+i.ordinal()+ " : " + judge(i)); 
+		//example : "Monday : 0 : Work" 
 	}
 }
--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 | ------------- | ------------- |
 | BaseDemo.java  | Base class   |
 | ConstructorTest.java | 	Test class constructor |
-| EqualsTest.java | Diff between 'equals' and '==' |
 | EnumTest.java | Test Enum in Java |
+| EqualsTest.java | Diff between 'equals' and '==' |
 | OtherTest.java | 	Some other matters needing attention |
 | RandomTest.java | Test Random Class in Java |

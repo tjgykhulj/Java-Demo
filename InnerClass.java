2655f66da53861d62c4cb99836257969c49da3cd
汤劲戈 <tjg-6666@163.com>
2016-02-24 09:47:18 +0800

    2016/2/24

--- InnerClass.java
+++ InnerClass.java
@@ -23,7 +23,22 @@ public class InnerClass extends BaseDemo {
 			}
 		};
 	}
-		
+	
+	// 继承类部类B的C，依然可以调用A的方法
+	static class A {
+		void show() { println("link succ");	}
+		class B {
+			A getOuter() { return A.this; }
+		}
+	}
+	// extends inner-class should make a outer class object
+	// in order to link outer-class
+	static class C extends A.B {
+		C(A x) {
+			x.super();
+			println("extends inner-class succ");
+		}
+	}
 	static public void main(String args[]) {		
 		InnerClass test = new InnerClass();
 		// .new to create a inner class
@@ -32,6 +47,11 @@ public class InnerClass extends BaseDemo {
 		
 		Contents x = test.getContents();
 		x.print();
+		
+		A a = new A();
+		C c = new C(a);
+		// to show that it can get A a
+		c.getOuter().show();
 	}
 	
 }

3c9c9c934ef7f34eb25740c55cf49c7a96aae8d7
汤劲戈 <tjg-6666@163.com>
2016-02-25 09:41:00 +0800

    2016/2/25

--- ArrayListTest.java
+++ ArrayListTest.java
@@ -2,10 +2,10 @@ package demo;
 
 import java.util.*;
 
+@SuppressWarnings({ "unchecked", "rawtypes" })
 public class ArrayListTest extends BaseDemo {
 	
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	static public void main(String a[]) 
+	static List testArrayList() 
 	{
 		List collection = new ArrayList(Arrays.asList(1,2,"JG", 3));
 		// add new items
@@ -24,6 +24,10 @@ public class ArrayListTest extends BaseDemo {
 		collection.removeAll(sub);
 		println(collection);
 		
+		return collection;
+	}
+	
+	static void testIterator(Collection collection) {
 		/*
 		 * Iterator can work with different types of collection
 		 * 因为无关容器，所以方便复用代码 ( next and remove )
@@ -37,5 +41,29 @@ public class ArrayListTest extends BaseDemo {
 			it.next(); it.remove();
 		}
 		println(collection);
+	}
+	
+	static void testListIterator(List collection) {
+		/*
+		 * ListIterator is a more powerful subtype of Iterator
+		 * that is product only by List class.
+		 * key : next\previous\set 
+		 */
+		ListIterator it = collection.listIterator();
+		while (it.hasNext()) print(it.next() + " ");
+		println(" [listIterator end]");
+		while (it.hasPrevious()) {
+			print(it.previous() + " ");
+			it.set(0);	//change all items to 0
+		}
+		println(" [listIterator end]");
+		println(collection);
+	}
+	
+	static public void main(String a[]) 
+	{
+		List arr = testArrayList();
+		testIterator(arr);
+		testListIterator(arr);
 	}
 }
--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # Java-Demo
 
-| File  | Description |
-| ------------- | ------------- |
-| ArrayListTest.java  | Test ArrayList and Iterator |
-| BaseDemo.java  | Base class   |
-| ClassTest.java | Features of class in Java |
-| ConstructorTest.java | Test class constructor |
-| EnumTest.java | Test Enum in Java |
-| EqualsTest.java | Diff between 'equals' and '==' |
-| InnerClass.java | Test inner class |
-| InterfaceTest.java | 	Interface Test |
-| OtherTest.java | 	Some other matters needing attention |
-| PolymorphismTest.java | Test polymorphism\abstract in Java |
-| RandomTest.java | Test Random Class in Java |
+| Level | File  | Description |
+| ---------- | ------------- | ------------- |
+| 0 | BaseDemo.java  | Base class   |
+| 0 | EnumTest.java | Test Enum in Java |
+| 0 | EqualsTest.java | Diff between 'equals' and '==' |
+| 0 | OtherTest.java | 	Some other matters needing attention |
+| 0 | RandomTest.java | Test Random Class in Java |
+| 1 | ClassTest.java | Features of class in Java |
+| 1 | ConstructorTest.java | Test class constructor |
+| 1 | InnerClass.java | Test inner class |
+| 1 | InterfaceTest.java | 	Interface Test |
+| 1 | PolymorphismTest.java | Test polymorphism\abstract in Java |
+| 2 | ArrayListTest.java  | Test ArrayList and Iterator |
+| 2 | StackDemo.java | Implements stack by LinkList |
